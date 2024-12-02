b7d9dae8f73c0467f1eb0a1bbe60fd2fe57b9a24
Andrii <88sushilnikov@gmail.com>
2024-12-03 00:27:34 +0200

    versino 5

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,39 +1,23 @@
-import { memo } from 'react'
 import css from './Options.module.css'
 
-const Options = memo(({ options, onLeaveFeedback, onReset, totalFeedback }) => {
-  const handleClick = (option) => {
-    requestAnimationFrame(() => {
-      onLeaveFeedback(option)
-    })
-  }
-
-  const handleReset = () => {
-    requestAnimationFrame(() => {
-      onReset()
-    })
-  }
-
+const Options = ({ options, onLeaveFeedback, onReset, totalFeedback }) => {
   return (
     <div className={css.options}>
       {options.map(option => (
         <button
           key={option}
-          onClick={() => handleClick(option)}
+          onClick={() => onLeaveFeedback(option)}
           type="button"
         >
           {option.charAt(0).toUpperCase() + option.slice(1)}
         </button>
       ))}
       {totalFeedback > 0 && (
-        <button type="button" onClick={handleReset}>
+        <button type="button" onClick={onReset}>
           Reset
         </button>
       )}
     </div>
   )
-})
-
-Options.displayName = 'Options'
-
+}
 export default Options
